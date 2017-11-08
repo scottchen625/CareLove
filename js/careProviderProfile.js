4559134365296828c72ec6eef263f56f71bd03fa
Scott Chen <scottchen625@gmail.com>
2017-11-07 22:25:19 -0800

    Add day of week availability to each profile

--- js/careProviderProfile.js
+++ js/careProviderProfile.js
@@ -2,6 +2,7 @@ var index = localStorage.getItem('personIndex');
 var jsonBlob = JSON.parse(localStorage.getItem('careTakerDataLocalStorage'));
 var careProviderData = jsonBlob[index - 1];
 
+careProviderData["availability"] = getAvailability(careProviderData);
 
 $(function () {
 
@@ -18,3 +19,25 @@ $(function () {
   parentDiv.append(html);
 
 });
+
+
+function getAvailability(careProviderData) {
+  var dateString = "";
+
+  if (careProviderData.Monday == 1)
+    dateString += "Monday ";
+  if (careProviderData.Tuesday == 1)
+    dateString += "Tuesday ";
+  if (careProviderData.Wednesday == 1)
+    dateString += "Wednesday ";
+  if (careProviderData.Thursday == 1)
+    dateString += "Thursday ";
+  if (careProviderData.Friday == 1)
+    dateString += "Friday ";
+  if (careProviderData.Saturday == 1)
+    dateString += "Saturday ";
+  if (careProviderData.Sunday == 1)
+    dateString += "Sunday";
+
+  return dateString;
+}
