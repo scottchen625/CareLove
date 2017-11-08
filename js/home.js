7c4390900fb5ec9cb065cd70f0e1bd3f22610445
migil <migil@ucsd.edu>
2017-11-07 17:38:32 -0800

    ready to push hamburger

--- js/home.js
+++ js/home.js
@@ -49,3 +49,14 @@ $(function () {
   	$(".caretakersNav").append(theTemplate(careTakerData));
 
 });
+
+
+/* Open the sidenav */
+function openNav() {
+    document.getElementById("mySidenav").style.display = "block";
+}
+
+/* Close/hide the sidenav */
+function closeNav() {
+    document.getElementById("mySidenav").style.display = "none";
+}
