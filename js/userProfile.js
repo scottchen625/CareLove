903dbffc5c9ef95d3b144c259815cf53110344ed
migil <migil@ucsd.edu>
2017-11-14 17:48:38 -0800

    user data updated

--- js/userProfile.js
+++ js/userProfile.js
@@ -1,31 +1,20 @@
-// Call this function when the page loads (the "ready" event)
-$(document).ready(function() {
-  console.log('hello world');
+$(function () {
+    var userData = [
+    {'fName':'Justin', 'lName' : 'Gil', 'age': 70, 'gender': 'M', 'phone': '949-111-1111', 'profilePic': 'pix/claudio.jpg', 'city' : 'San Diego', 'add' : '55 Serenity'
+    , 'Bio': 'I like soccer and long walks on the beach.', 'userIndex' : 1},
+    {'fName':'Momin', 'lName' : 'Kahn', 'age': 60, 'gender': 'M', 'phone': '949-111-1312', 'profilePic': 'pix/claudio.jpg', 'city' : 'Carlsbad', 'add' : '23 Rollings'
+    , 'Bio': 'I like soccer and long walks on the beach.', 'userIndex' : 2},
+    {'fName':'Scott', 'lName' : 'Chen', 'age': 50, 'gender': 'M', 'phone': '949-111-1113', 'profilePic': 'pix/claudio.jpg', 'city' : 'Oceanside', 'add' : '35 Jasmin'
+    , 'Bio': 'I like soccer and long walks on the beach.', 'userIndex' : 3},
+    {'fName':'Charles', 'lName' : 'Chen', 'age': 72, 'gender': 'M', 'phone': '949-111-1114', 'profilePic': 'pix/claudio.jpg', 'city' : 'Coronado', 'add' : '388 Rose'
+    , 'Bio': 'I like soccer and long walks on the beach.', 'userIndex' : 4},
+    {'fName':'Philip', 'lName' : 'Guo', 'age': 74, 'gender': 'M', 'phone': '949-111-1115', 'profilePic': 'pix/claudio.jpg', 'city' : 'Solana Beach', 'add' : '213 Wizzle'
+    , 'Bio': 'I like soccer and long walks on the beach.', 'userIndex' : 5},
+    ]
 
-  // when you first load the page, set a custom name if you have one:
-  var name = localStorage.getItem('customName');
-  if (name) {
-    $("#fName").html(name);
-  }
-
-  // use localStorage to store your name
-  $("#changeName").click(function() {
-    var newName = prompt("What's your new name?");
-    if (newName) {
-      $("#fName").html(newName);
-      localStorage.setItem('fName', newName);
-    }
-  });
-})
+    localStorage.setItem('userDataLocalStorage', JSON.stringify(userData));
+}
 
 
-/*
-localStorage.setItem('careTakerDataLocalStorage', JSON.stringify(careTakerData));
-	var fName = localStorage.getItem('fName');
-	var lName = localStorage.getItem('lName');
-	var address = localStorage.getItem('address');
-	var city = localStorage.getItem('city');
-	var state = localStorage.getItem('state');
-	var zip = localStorage.getItem('zip');
-	var phone = localStorage.getItem('phone');
-*/
+);
+
